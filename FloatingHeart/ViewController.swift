c7be65f70d1b0e7a7be7c15554268c669637f770
saidmarouf <saidmmm2002@saids-macbook-pro-retina.local>
2015-09-22 10:18:42 +0200

    Initial Commit

---
+++ FloatingHeart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FloatingHeart
+//
+//  Created by Said Marouf on 9/22/15.
+//  Copyright © 2015 Said Marouf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
